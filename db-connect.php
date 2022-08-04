c1f815b0be3d27757eb6b45b68250e7aba2a246f
meraklLeo <110579203+meraklLeo@users.noreply.github.com>
2022-08-04 11:24:02 +0900

    Add files via upload

---
+++ db-connect.php
@@ -0,0 +1,12 @@
+<?php
+    $host     = 'localhost';
+    $username = 'root';
+    $password = '';
+    $dbname   ='my_db';
+
+    $conn = new mysqli($host, $username, $password, $dbname);
+    
+    if(!$conn){
+        die("Cannot connect to the database.". $conn->error);
+    }
+?>    
